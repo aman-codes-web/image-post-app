1a29a4d9377381cde54cfa56aa9ef300b289139f
Aman <amansah684@gmail.com>
2026-05-26 14:40:55 +0530

    fix: syntax error in App.jsx

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-kimport React from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Createpost } from './pages/Createpost'
 import Feed from './pages/Feed'
